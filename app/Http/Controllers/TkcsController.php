16e1c2d21bb6a4e01cb92a73eb4f07648a124d55
Michal Plachy <plachym.it@gmail.com>
2018-09-06 13:11:45 +0700

    fix x

--- app/Http/Controllers/TkcsController.php
+++ app/Http/Controllers/TkcsController.php
@@ -12,6 +12,7 @@ use Auth;
 use DateTime;
 use App\User;
 use App\BieuStatus;
+use App\Bieumau1;
 
 class TkcsController extends Controller
 {
@@ -20,6 +21,37 @@ class TkcsController extends Controller
         $lists = User::listYearAdmin();
         
         return view('admin.tkcs.index', ['lists' => $lists]);
+    }
+
+    public function search($year)
+    {
+        $input = Input::all();
+        $userIds = BieuStatus::where('status','>=', 1)->where('year', $year)->pluck('user_id');
+        $bieumau = Bieumau1::whereIn('user_id', $userIds)
+            ->where('manager', 'LIKE', "%".$input['manager']."%")
+            ->where('manager_city', 'LIKE', "%".$input['manager_city']."%");
+
+        if($input['establish_lever'] != 0)
+            $bieumau = $bieumau->where('establish_lever', $input['establish_lever']);
+        if($input['type_company'] != 0)
+            $bieumau = $bieumau->where('type_company', $input['type_company']);
+        if($input['type_econom'] != 0)
+            $bieumau = $bieumau->where('type_econom', $input['type_econom']);
+        if($input['lab_number_sub'] != 0)
+            $bieumau = $bieumau->where('lab_number_sub', $input['lab_number_sub']);
+            $user_ids = $bieumau->pluck('user_id');
+        $users = User::whereIn('id', $user_ids)->get();
+
+        if($input['manager'] == "" && 
+            $input['manager_city'] == "" && 
+            $input['establish_lever'] == 0 && 
+            $input['type_company'] == 0 && 
+            $input['type_econom'] == 0 && 
+            $input['lab_number_sub'] == 0
+        )
+        $users = User::getAllUserHasReportedAdmin($year)->get();
+    
+        return view('admin.tkcs.year', ['users' => $users, 'year' => $year]);
     }
 
     public function year($year)
--- resources/views/admin/tkcs/year.blade.php
+++ resources/views/admin/tkcs/year.blade.php
@@ -19,6 +19,95 @@
             </div>
             <!-- /.box-header -->
             <div class="box-body">
+              <div class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                        Tìm kiếm nâng cao
+                    </div>
+                    <div class="collapse" id="collapseExample">
+                        <div class="card card-body">
+                            <br>
+                            <form role="form" method="POST" action="{{ url('/admin/tkcs/'.$year.'/search') }}">
+                                {{ csrf_field() }}
+                                <div class="form-group row">
+                                    <div class="col-md-6">
+                  <label for="manager">Cơ quan quản lý trực tiếp:</label>
+                  <input type="text" name="manager" class="form-control" id="manager">
+                  <label for="manager_city">Bộ/ngành,tỉnh/tp trực thuộc TƯ chủ quản :</label>
+                  <input type="text" name="manager_city" class="form-control" id="manager_city">
+                </div></div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <select class="form-control" name="establish_lever">
+                                                <option value="0">Cấp quyết định thành lập:</option>
+                                                <option value="1">Quốc hội, Uỷ ban thường vụ Quốc hội</option>
+                                                <option value="2">Chính phủ</option>
+                                                <option value="3">Toà án nhân dân tối cao</option>
+                                                <option value="4">Viện kiểm sát nhân dân tối cao</option>
+                                                <option value="5">Thủ tướng Chỉnh phủ</option>
+                                                <option value="6">Bộ trưởng, Thủ trưởng cơ quan ngang Bộ, cơ quan thuộc Chính phủ</option>
+                                                <option value="7">Uỷ ban nhân dân tỉnh, thành phố trực thuộc Trung ương</option>
+                                                <option value="8">Tổ chức chính trị, tổ chức chính trị - xã hội, tổ chức xã hội, tổ chức xã hội - nghề nghiệp</option>
+                                                <option value="9">Doanh nghiệp, tổ chức khác, cá nhân</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <select class="form-control" name="type_company">
+                                                <option value="0">Loại hình của đơn vị/tổ chức :</option>
+                                                <option value="1">Cơ quan quản lý nhà nước</option>
+                                                <option value="2">Tổ chức nghiên cứu khoa học và phát triển công nghệ</option>
+                                                <option value="3">Cơ sở giáo dục đại học</option>
+                                                <option value="4">-----Đại học</option>
+                                                <option value="5">-----Học viện</option>
+                                                <option value="6">-----Cao đẳng</option>
+                                                <option value="7">Tổ chức dịch vụ KH&CN</option>
+                                                <option value="8">-----DV thông tin, thư viện</option>
+                                                <option value="9">-----DV bảo tàng cho KH&CN</option>
+                                                <option value="10">-----DV dịch thuật, biên tập, xuất bản cho KH&CN</option>
+                                                <option value="11">-----DV điều tra cơ bản định kỳ, thường xuyên</option>
+                                                <option value="12">-----DV thống kê, điều tra xã hội</option>
+                                                <option value="13">-----DV tiêu chuẩn đo lường chất lượng</option>
+                                                <option value="14">-----DV tư vấn về KH&CN</option>
+                                                <option value="15">-----DV sở hữu trí tuệ</option>
+                                                <option value="16">-----DV chuyển giao công nghệ</option>
+                                                <option value="17">-----DV KH&CN khác</option>
+                                                <option value="18">Đơn vị sự nghiệp khác</option>
+                                                <option value="19">Doanh nghiệp KH&CN</option>
+                                                <option value="20">Doanh nghiệp</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <select class="form-control" name="type_econom">
+                                                <option value="0">Loại hình kinh tế :</option>
+                                                <option value="1"> Nhà nước</option>
+                                                <option value="2">Ngoài nhà nước</option>
+                                                <option value="3">Có vốn đầu tư nước ngoài</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <select name="lab_number_sub" class="form-control">
+                                                <option value="0">Lĩnh vực nghiên cứu chính theo chức năng nhiệm vụ :</option>
+                                                <option value="1">1. Khoa học tự nhiên</option>
+                                                <option value="2">2. Khoa học kỹ thuật và công nghệ</option>
+                                                <option value="3">3. Khoa học y dược</option>
+                                                <option value="4">4. Khoa học nông nghiệp</option>
+                                                <option value="5">5. Khoa học xã hội</option>
+                                                <option value="6">6. Khoa học nhân văn</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary right">Tìm kiếm</button>
+                            </form>
+                        </div>
+                    </div>
               <table id="example2" class="table table-bordered table-hover">
                 <thead>
                 <tr>
